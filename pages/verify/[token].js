bb38ee10a4ca4f75c1ae8f0e6340fd125d2617f1
mysamy <47317673+mysamy@users.noreply.github.com>
2025-10-24 10:42:04 +0200

    a

--- pages/verify/[token].js
+++ pages/verify/[token].js
@@ -11,10 +11,10 @@ export default function VerifyPage() {
 
     const verifyEmail = async () => {
       try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/verify`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ token }),
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/verify`,
+          {
+          method: "GET",
         });
 
         const data = await res.json();
